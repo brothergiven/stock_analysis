bb3145275e97eb28e5c3b204934036083225a37e
brothergiven <dung3740@gmail.com>
2025-04-18 03:03:52 +0900

    feat: DB 스키마 수정

--- db/database.py
+++ db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+from contextlib import contextmanager
 
 load_dotenv()  
 DB_URL = os.getenv("DB_URL", "mysql+pymysql://root:password@localhost:3306/stock_db")
@@ -14,7 +15,18 @@ Base = declarative_base()
 
 # DB 초기화 (최초 1회 사용)
 def init_db():
-    from models import CompanyInfo, MarketDaily, TradingByInvestorDaily
+    from db.models import CompanyInfo, DailyPrice, TradingByInvestorDaily
     # DB 스키마 생성
     Base.metadata.create_all(bind=engine)
 
+@contextmanager
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()  
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
--- db/models.py
+++ db/models.py
@@ -2,48 +2,35 @@
 DB 정의
 """
 
-from sqlalchemy import Column, String, Integer, BigInteger, Float, Date
-from database import Base
+from sqlalchemy import Column, String, Integer, BigInteger, Float, DateTime, Date
+from db.database import Base
+from datetime import datetime
 
 
 # 회사 정보
 class CompanyInfo(Base):
     __tablename__ = 'company_info'
-    code = Column(String(6), primary_key=True) # 종목 코드
-    company = Column(String(100)) # 회사 이름
-    market = Column(String(10))  # KOSPI, KOSDAQ, KONEX
-    last_update = Column(Date) # 마지막으로 정보가 갱신된 날짜
-
+    code = Column(String(10), primary_key=True, comment="종목 코드")
+    name = Column(String(50), nullable=False, comment="종목명")
+    market = Column(String(20), nullable=False, comment="대표 시장명 (예: KOSPI, KOSDAQ)")
+    industry = Column(String(50), nullable=False, comment="업종명")
+    capital = Column(BigInteger, nullable=True, comment="자본금 (억원)")
+    shares = Column(BigInteger, nullable=True, comment="상장 주식 수")
+    fiscal_month = Column(Integer, nullable=True, comment="결산월 (1~12)")
+    created_at = Column(DateTime, default=datetime.now(), nullable=False, comment="데이터 생성 시각")
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False, comment="데이터 수정 시각")
 
 # 일간 데이터
-class MarketDaily(Base):
-    __tablename__ = 'market_daily'
-    code = Column(String(6), primary_key=True) # 종목코드(PK)
-    date = Column(Date, primary_key=True) # 거래일(PK)
+class DailyPrice(Base):
+    __tablename__ = "daily_price"
 
-    open = Column(Integer) # 시가
-    high = Column(Integer) # 고가
-    low = Column(Integer) # 저가
-    close = Column(Integer) # 종가
-    volume = Column(BigInteger) # 거래량
-    change_rate = Column(Float) # 등락률
-
-    bps = Column(Integer) # 주당순자산
-    per = Column(Float) # 주가수익비율
-    pbr = Column(Float) # 주가순자산비율
-    eps = Column(Integer) # 주당순이익
-    div = Column(Float) # 배당수익률
-    dps = Column(Integer) # 주당배당금
-
-    market_cap = Column(BigInteger) # 시가총액
-    trade_value = Column(BigInteger) # 거래대금
-    shares = Column(BigInteger) # 상장주식수
-
-    foreign_shares = Column(BigInteger) # 외국인 보유 주식 수
-    foreign_ratio = Column(Float) # 외국인 지분율(%)
-    limit_shares = Column(BigInteger) # 외국인 한도 주식 수
-    limit_ratio = Column(Float) # 외국인 한도 소진률
-
+    date = Column(Date, primary_key=True, comment="거래일")
+    open = Column(Integer, nullable=False, comment="시가")
+    high = Column(Integer, nullable=False, comment="고가")
+    low = Column(Integer, nullable=False, comment="저가")
+    close = Column(Integer, nullable=False, comment="종가")
+    volume = Column(BigInteger, nullable=False, comment="거래량")
+    code = Column(String(10), primary_key=True, comment="종목코드")
 
 
 # 개인, 외국인, 기관별 거래 추이
